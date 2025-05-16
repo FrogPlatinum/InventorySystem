9003422c5acb878f18db8561cdd4e3b293c0f1d2
Gavin Denver <gavindenver@gmail.com>
2025-05-16 12:00:15 +0200

    Added Sample Inventory to MemoryItemRepo

--- InventorySystem/Repos/MemoryItemRepo.cs
+++ InventorySystem/Repos/MemoryItemRepo.cs
@@ -13,6 +13,48 @@ namespace InventorySystem.Repos
         //Memory list for items
         private readonly List<Item> _items = new List<Item>();
 
+        //IdGenerator
+        IdGenerator idGenerator = new IdGenerator();
+
+        public MemoryItemRepo()
+        {
+            //Sample Book 1
+            string book1Id = idGenerator.GenerateId("Bk");
+            Book bookToAdd1 = new Book(book1Id,"Dune", Item.Condition.New, Item.NeedsApproval.No, Item.StorageStatus.Yes, "Frank Herbert", "1st", "Literature");
+            _items.Add(bookToAdd1);
+
+            //Sample Book 2
+            string book2Id = idGenerator.GenerateId("Bk");
+            Book bookToAdd2 = new Book(book2Id, "Three Body Problem", Item.Condition.Used, Item.NeedsApproval.No, Item.StorageStatus.Yes, "Cixin Liu", "2nd", "Literature");
+            _items.Add(bookToAdd2);
+
+            //Sample Book 3
+            string book3Id = idGenerator.GenerateId("Bk");
+            Book bookToAdd3 = new Book(book3Id, "Players Handbook", Item.Condition.Good, Item.NeedsApproval.Yes, Item.StorageStatus.Yes, "WoTC", "5th", "DnD");
+            _items.Add(bookToAdd3);
+
+            //Sample Boardgame 1
+            string bg1Id = idGenerator.GenerateId("bg");
+            BoardGame gameToAdd1 = new BoardGame(bg1Id, "Radlands", Item.Condition.Used, Item.NeedsApproval.No, Item.StorageStatus.Yes, "2nd", 2, 2);
+            _items.Add(gameToAdd1);
+
+            //Sample Boardgame 2
+            string bg2Id = idGenerator.GenerateId("bg");
+            BoardGame gameToAdd2 = new BoardGame(bg2Id, "Twilight Imperium", Item.Condition.Damaged, Item.NeedsApproval.Yes, Item.StorageStatus.No, "4th", 2, 8);
+            _items.Add(gameToAdd2);
+
+            //Sample LiveEquipment 1
+            string le1Id = idGenerator.GenerateId("le");
+            LiveEquipment leToAdd1 = new LiveEquipment(le1Id, "Plate Armor", Item.Condition.New, Item.NeedsApproval.Yes, Item.StorageStatus.Yes, LiveEquipment.TypeOfEquipment.Costume, "Cainhurst Vilebloods");
+            _items.Add(leToAdd1);
+
+            //Sample LiveEuipment 2
+            string le2Id = idGenerator.GenerateId("le");
+            LiveEquipment leToAdd2 = new LiveEquipment(le2Id, "Greatsword", Item.Condition.Used, Item.NeedsApproval.No, Item.StorageStatus.No, LiveEquipment.TypeOfEquipment.Weapon, "The Blood Beast");
+            _items.Add(leToAdd2);
+
+        }
+
         //Add item to list
         public void Add(Item item)
         {
